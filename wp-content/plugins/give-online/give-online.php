58706a04fb5561a85f2bdb0ca232b7041876704a
Eli Perelman <eli@eliperelman.com>
2012-04-03 17:31:49 -0500

    Give Online plugin changes

--- wp-content/plugins/give-online/give-online.php
+++ wp-content/plugins/give-online/give-online.php
@@ -7,3 +7,22 @@ Author: Eli Perelman
 Author URI: http://eliperelman.com
 */
 ?>
+error_reporting(E_ALL);
+
+add_action("widgets_init", array('GiveOnline', 'register'));
+
+class GiveOnline {
+  function control(){
+    echo 'I am a control panel';
+  }
+  function widget($args){
+    echo $args['before_widget'];
+    echo $args['before_title'] . 'Your widget title' . $args['after_title'];
+    echo 'I am your widget';
+    echo $args['after_widget'];
+  }
+  function register(){
+    register_sidebar_widget('Give Online', array('GiveOnline', 'widget'));
+    register_widget_control('GiveOnline', array('GiveOnline', 'control'));
+  }
+}
